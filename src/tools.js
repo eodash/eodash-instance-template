90f252ab9d9b8fd7afad41422506a6e6e89c53df
A-Behairi <behairiahmed@gmail.com>
2024-03-21 08:06:50 +0100

    support internal widgets

--- src/tools.js
+++ src/tools.js
@@ -1,38 +1,10 @@
-/** @type {import("@eodash/eodash").WebComponentWidget} */
+/** @type {import("@eodash/eodash").InternalComponentWidget} */
 export default {
   id: Symbol(),
   title: "Tools",
   layout: { x: 0, y: 0, w: 3, h: 12 },
   widget: {
-    link: "https://cdn.skypack.dev/@eox/itemfilter",
-    properties: {
-      config: {
-        titleProperty: "title",
-        filterProperties: [
-          {
-            keys: ["title", "themes"],
-            title: "Search",
-            type: "text",
-            expanded: true,
-          },
-          {
-            key: "themes",
-            title: "Theme",
-            type: "multiselect",
-            featured: true,
-          },
-        ],
-        aggregateResults: "themes",
-        enableHighlighting: true,
-      },
-    },
-    onMounted: async function (el, store, _) {
-      /** @type {any} */ (el).apply(store?.stac);
-      /** @type {any} */ (el).config.onSelect = async (item) => {
-        await store.loadSelectedSTAC(item.href);
-      };
-    },
-    tagName: "eox-itemfilter",
+    name:"List"
   },
-  type: "web-component",
+  type: "internal",
 };
