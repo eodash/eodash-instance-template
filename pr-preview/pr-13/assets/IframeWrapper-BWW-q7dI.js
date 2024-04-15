61f5426c3c7202c5eb9a943e6896da4165d78070
santilland <santilland@users.noreply.github.com>
2024-04-15 11:37:23 +0000

    Remove preview for PR 13 🛬

--- pr-preview/pr-13/assets/IframeWrapper-BWW-q7dI.js
+++
@@ -1,1 +0,0 @@
-import{q as r,t}from"./index-1JPqI4wf.js";const s=["src"],o={__name:"IframeWrapper",props:{src:{type:String,required:!0}},setup(e){return(c,a)=>(r(),t("iframe",{src:e.src,style:{width:"100%",height:"100%"}},null,8,s))}};export{o as default};
