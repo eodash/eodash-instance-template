3e1559f8b530fae73e5a28ad68065cdfec390afc
A-Behairi <behairiahmed@gmail.com>
2024-05-27 12:08:43 +0200

    feat: added a hidden widget example with comments

--- src/information.js
+++ src/information.js
@@ -2,26 +2,38 @@ import { store } from "@eodash/eodash";
 
 const { currentUrl } = store.states;
 /**
- * @type {import("@eodash/eodash").Widget}
+ * STAC information panel
+ * @type {import("@eodash/eodash").FunctionalWidget}
  **/
 export default {
-  id: "Information",
   layout: { x: 9, y: 0, w: 3, h: 12 },
-  title: "Information",
-  type: "web-component",
-  widget: {
-    link: () => import("@eox/stacinfo"),
-    tagName: "eox-stacinfo",
-    properties: {
-      for: currentUrl,
-      allowHtml: "true",
-      styleOverride:
-        "#properties li > .value {font-weight: normal !important;}",
-      header: "[]",
-      subheader: "[]",
-      properties: '["description"]',
-      featured: "[]",
-      footer: "[]",
-    },
+  defineWidget: (selectedSTAC) => {
+    // show "eox-stacinfo" web component if a stac object is selected and
+    // hide the panel if not
+    return selectedSTAC
+      ? {
+          id: "Information",
+          title: "Information",
+          type: "web-component",
+          widget: {
+            // importing an installed dependency as a web component definition
+            link: () => import("@eox/stacinfo"),
+            tagName: "eox-stacinfo",
+            // assigning `eox-stacinfo` attributes and properties
+            properties: {
+              // assigning currently selected endpoint from the store
+              for: currentUrl,
+              allowHtml: "true",
+              styleOverride:
+                "#properties li > .value {font-weight: normal !important;}",
+              header: "[]",
+              subheader: "[]",
+              properties: '["description"]',
+              featured: "[]",
+              footer: "[]",
+            },
+          },
+        }
+      : null;
   },
 };
