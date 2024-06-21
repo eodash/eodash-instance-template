a6d3a74f31d05f1251929db51fb86f0c382d541d
A-Behairi <behairiahmed@gmail.com>
2024-06-21 11:55:33 +0200

    chore: update to v2.5 and remove svelte zoom-btn

--- eodash.config.js
+++ eodash.config.js
@@ -1,16 +1,14 @@
-/** @type {import("@eodash/eodash").EodashCLiConfig} */
-export default {
+import { defineConfig } from "@eodash/eodash/config";
+export default defineConfig({
   base: "",
   dev: {
-    port: 3001,
+    port: 3000,
     host: false,
     open: false,
   },
   preview: {
-    port: 4173,
     host: true,
     open: true,
   },
-  outDir: ".eodash/dist",
   cacheDir: ".eodash/temp",
-};
+});
