f6bfc789086870d4c068c911269719bbfa41205c
A-Behairi <A-Behairi@users.noreply.github.com>
2024-07-04 07:27:54 +0000

    Deploy preview for PR 17 🛫

---
+++ pr-preview/pr-17/assets/EodashItemFilter-BrZ-eyrX.js
@@ -0,0 +1,10 @@
+import{$ as p,a0 as c,at as d}from"./index-B5c5Pwhg.js";import m from"./DynamicWebComponent-CFpMl29n.js";const f={__name:"EodashItemFilter",setup(u){const r=()=>d(()=>import("./eox-itemfilter-B6N0Xfw2.js"),[]),o={config:{titleProperty:"title",filterProperties:[{keys:["title","themes"],title:"Search",type:"text"},{key:"themes",title:"Theme Filter",type:"multiselect"}],aggregateResults:"themes",enableHighlighting:!0,expandMultipleFilters:!1,expandMultipleResults:!1}},a=(t,i)=>{t.style.height="100%";const s=document.createElement("style");s.innerHTML=`
+    section {
+      margin: 0 !important;
+    }
+    section button#filter-reset {
+      padding: 0 8px;
+      top: 8px;
+      right: 8px;
+    }
+  `,t?.shadowRoot?.appendChild(s);const l=document.createElement("div");l.setAttribute("slot","filterstitle"),l.innerHTML='<h4 style="margin: 14px 8px">Indicators</h4>',t.appendChild(l);const n=document.createElement("div");n.setAttribute("slot","resultstitle"),t.appendChild(n),t.apply(i.stac?.filter(e=>e.rel==="child")),t.config.onSelect=async e=>{console.log(e),await i.loadSelectedSTAC(e.href)}};return(t,i)=>(p(),c(m,{link:r,"tag-name":"eox-itemfilter",properties:o,"on-mounted":a}))}};export{f as default};
--- pr-preview/pr-17/assets/ssrBoot-CqR98f8O.js
+++
@@ -1,1 +0,0 @@
-import{s,Z as t,c as e,aM as n}from"./index-DBoWNDl8.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
