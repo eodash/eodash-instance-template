41ea9a33dfef27474c7c0a127aa9c93201e0b4c5
A-Behairi <A-Behairi@users.noreply.github.com>
2024-08-02 12:31:39 +0000

    Deploy preview for PR 18 🛫

--- pr-preview/pr-18/assets/DashboardLayout-C73rv7AI.js
+++ pr-preview/pr-18/assets/DashboardLayout-C73rv7AI.js
@@ -1,4 +1,4 @@
-import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as v,ab as w}from"./index-BJoh-Mee.js";import{V as k}from"./VMain-DR2jJpL2.js";import"./ssrBoot-Pxg2E6tT.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
+import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as v,ab as w}from"./index-DrAyEfuK.js";import{V as k}from"./VMain-ggA5vR4o.js";import"./ssrBoot-D9Bc4vkQ.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
     <style>
       :host {
         display: grid;
