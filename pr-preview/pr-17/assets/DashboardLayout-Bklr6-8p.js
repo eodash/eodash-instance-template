ce9e54a4b5bcedae8d62ec7a8b1e43e8ad8b6254
A-Behairi <A-Behairi@users.noreply.github.com>
2024-08-05 15:38:21 +0000

    Deploy preview for PR 17 🛫

--- pr-preview/pr-17/assets/DashboardLayout-Bklr6-8p.js
+++ pr-preview/pr-17/assets/DashboardLayout-Bklr6-8p.js
@@ -1,4 +1,4 @@
-import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as v,ab as w}from"./index-BHzPgH2J.js";import{V as k}from"./VMain-CoJ_6Ucg.js";import"./ssrBoot-7hoALQc5.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
+import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as v,ab as w}from"./index-Dvu417lt.js";import{V as k}from"./VMain-CoSaaYTf.js";import"./ssrBoot-Uu_-yAhn.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
     <style>
       :host {
         display: grid;
