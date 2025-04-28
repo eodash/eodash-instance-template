2b7327c7efb679547e3d9576c63332958b1777b4
A-Behairi <A-Behairi@users.noreply.github.com>
2025-04-28 10:22:03 +0000

    Deploy preview for PR 20 🛫

--- pr-preview/pr-20/assets/unsafe-html-4xauUBrQ.js
+++
@@ -1,9 +0,0 @@
-import{E as r,T as o}from"./lit-element-BrwkOUt4.js";import{e as c,i as u,t as a}from"./directive-DPIcWbGP.js";/**
- * @license
- * Copyright 2021 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function*h(e,t){if(e!==void 0){let i=0;for(const n of e)yield t(n,i++)}}/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */class s extends u{constructor(t){if(super(t),this.it=r,t.type!==a.CHILD)throw Error(this.constructor.directiveName+"() can only be used in child bindings")}render(t){if(t===r||t==null)return this._t=void 0,this.it=t;if(t===o)return t;if(typeof t!="string")throw Error(this.constructor.directiveName+"() called with a non-string value");if(t===this.it)return this._t;this.it=t;const i=[t];return i.raw=i,this._t={_$litType$:this.constructor.resultType,strings:i,values:[]}}}s.directiveName="unsafeHTML",s.resultType=1;const l=c(s);export{l as a,h as o};
